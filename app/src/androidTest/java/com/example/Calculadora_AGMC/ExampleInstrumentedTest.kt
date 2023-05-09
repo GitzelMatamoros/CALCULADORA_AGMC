585c47ce9e5324dc2415514cc24432d84d8217be
Gitzel <matamoros.castro.america.c.m@gmail.com>
2023-05-09 10:09:28 -0600

    se subio proyecto

---
+++ app/src/androidTest/java/com/example/Calculadora_AGMC/ExampleInstrumentedTest.kt
@@ -0,0 +1,19 @@
+package com.example.Calculadora_AGMC
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.codingformobile.calculator", appContext.packageName)
+    }
+}
